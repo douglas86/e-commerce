dae5457f0ef501f5f2fe3dac98f072c830610d84
douglas86 <douglasmaxton@gmail.com>
2020-09-14 20:40:38 +0200

    added detail.html template

--- shop/views.py
+++ shop/views.py
@@ -13,7 +13,12 @@ def index(request):
 
     #  paginator code
     paginator = Paginator(product_objects, 4)
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     product_objects = paginator.get_page(page)
 
     return render(request, "shop/index.html", {"product_objects": product_objects})
+
+
+def detail(request, id):
+    product_object = Product.objects.get(id=id)
+    return render(request, "shop/detail.html", {"product_object": product_object})
