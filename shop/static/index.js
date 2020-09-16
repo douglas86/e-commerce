b835a6bf2180fbc02c46c82f1a924934da76d909
douglas86 <douglasmaxton@gmail.com>
2020-09-16 21:19:24 +0200

    stored items in localStorage

--- shop/static/index.js
+++ shop/static/index.js
@@ -7,4 +7,11 @@ if (localStorage.getItem("cart") == null) {
 $(document).on("click", ".atc", function () {
   var item_id = this.id.toString();
   console.log(item_id);
+  if (cart[item_id] != undefined) {
+    cart[item_id] = cart[item_id] + 1;
+  } else {
+    cart[item_id] = 1;
+  }
+	console.log(cart)
+	localStorage.setItem("cart", JSON.stringify(cart))
 });
