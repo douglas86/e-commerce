9b343ce79e57346bebd9572abae53ab648bc5dea
douglas86 <douglasmaxton@gmail.com>
2020-09-17 14:13:51 +0200

    reloading page on button click

--- shop/static/index.js
+++ shop/static/index.js
@@ -1,27 +1,29 @@
+// get localStorage item cart
+// if doesn't exist create it
 if (localStorage.getItem("cart") == null) {
   var cart = {};
 } else {
   cart = JSON.parse(localStorage.getItem("cart"));
 }
 
+// once item is clicked
 $(document).on("click", ".atc", function () {
   var item_id = this.id.toString();
-  // console.log(item_id);
-  console.log(item_id);
   if (cart[item_id] != undefined) {
     cart[item_id] = cart[item_id] + 1;
   } else {
     cart[item_id] = 1;
   }
-  // console.log(cart);
-  console.log(cart);
   localStorage.setItem("cart", JSON.stringify(cart));
   document.getElementById("cart").innerHTML =
     "Cart(" + Object.keys(cart).length + ")";
+  location.reload();
 });
 
+// run function
 displayCart(cart);
 
+// create items in popover menu
 function displayCart(cart) {
   cartString = "";
   cartString += "<h5>This is the cart</h5>";
@@ -37,12 +39,13 @@ function displayCart(cart) {
   $('[data-toggle="popover"]').popover();
 }
 
+// load function when doc starts
 function loadFun(cart) {
-  console.log("I am here");
   for (var x in cart) {
     document.getElementById("cart").innerHTML =
       "Cart(" + Object.keys(cart).length + ")";
   }
 }
 
+// start loading on windows start
 $(window).on("load", loadFun(cart));
